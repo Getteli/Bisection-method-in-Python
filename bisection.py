bfda25c89f6b4a1f8c6d4da6fdeaa0519ed13524
Getteli <38441697+Getteli@users.noreply.github.com>
2022-03-12 11:13:49 -0300

    mudancas para o exercicio no tema 3

--- bisection.py
+++ bisection.py
@@ -5,34 +5,50 @@ import math
 def func(x):
     return math.cos(x) - x
 
+def equa(x):
+    # 3x^4 - 2x^3 - x^2 + x + 8
+    return 3*x**4 - 2*x**3 - x**2 + x + 8
 
 def main():
-    a = 0.0
-    b = 1.0
-    tolerance = 0.00001
+    a = -1 #1
+    b = 1 #2
+    tolerance = 0.000001
     if len(sys.argv) < 2 or len(sys.argv) > 2:
         print 'Usage : bisection.py <maximum iterations>'
         sys.exit(0)
 
     
-    print 'iteration\t a\t\t b\t\t c\t\t f(c)\t'
-    print '---------\t --------\t --------\t --------\t --------\t'
+    print 'iteracao\t a\t\t b\t\t media\t\t f(a)\t\t f(media)\t sinal\t\t error(b-a)\t'
+    print '---------\t --------\t --------\t --------\t --------\t --------\t --------\t --------\t'
     i = 0
     while i <= int(sys.argv[1]):
-        c = (a + b)/2.0
-        if (func(a) * func(c)) < 0: #sign checking
+
+        fa = equa(a)
+        c = (a + b)/2.0 #3
+        fc = equa(c)
+        error = (b - a)
+
+        if(i == 0):
+            s = "+"
+            error = 0
+
+        print "%d\t\t %.6f\t %.6f\t %.6f\t %.6f\t %.6f\t %s\t\t %.6f\t" %(i ,a ,b ,c, fa , fc, s, error)
+
+        if (equa(a) * equa(c)) < 0:
             b = c
-        else: #(func(b) * func(c)) > 0
+            s = "-"
+        else:
             a = c
-        i = i + 1
-        print "%d\t\t %.6f\t %.6f\t %.6f\t %.6f" %(i-1 ,a ,b ,c ,func(c))
+            s = "+"
+
+        i = i + 1 
 
 
-#we now get out of loop and print the value of final c
+#agora saimos do loop e imprimimos o valor da media final
     if abs((a-b)/2.0) < tolerance:
-        print 'The root after %dth iteration is %.10f.'%(i-1,c) 
+        print 'A raiz depois de %d iteracoes e %.10f.'%(i-1,c) 
     else:
-        print 'We were unable to meet tolerance within given iterations.'
+        print 'Nao foi possivel atender a tolerancia em determinadas iteracoes.'
     
 
 
